29106b1e60050e086b51040a65cb6f566bea73c8
legacy-ux <maintenance@altampa.com>
2026-08-05 15:16:25 -0400

    Export Task model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,14 @@
 
 from app.models.director import Director, DirectorKey
 from app.models.specialist import Specialist, SpecialistKey
+from app.models.task import Task, TaskPriority, TaskStatus
 
-__all__ = ["Director", "DirectorKey", "Specialist", "SpecialistKey"]
+__all__ = [
+    "Director",
+    "DirectorKey",
+    "Specialist",
+    "SpecialistKey",
+    "Task",
+    "TaskPriority",
+    "TaskStatus",
+]
