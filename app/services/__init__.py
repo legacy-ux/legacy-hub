0667bf889e524952cd5fdedc2b9eefd25ba9102e
legacy-ux <maintenance@altampa.com>
2026-08-05 15:52:26 -0400

    Add Director Registry API service

---
+++ app/services/__init__.py
@@ -0,0 +1,5 @@
+"""Application services for Legacy Hub."""
+
+from app.services.directors import get_director, list_directors, synchronize_directors
+
+__all__ = ["get_director", "list_directors", "synchronize_directors"]
